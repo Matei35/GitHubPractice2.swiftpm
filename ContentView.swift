a577fc5051a510e426268060e12ed0794c58fab2
Matei Donisa <mdonisa8105@stu.d214.org>
2025-04-11 13:24:08 -0500

    added the text field that connects to number 2

--- ContentView.swift
+++ ContentView.swift
@@ -1,8 +1,8 @@
 import SwiftUI
 struct ContentView: View {
-    @State var number1: Int
+    @State var number1 = 0
     @State var number2 = 0
     var body: some View {
-        TextField("Enter Number", value: $number1, format: .number)
+        TextField("enter a number", value: $number2, format: .number)
     }
 }
