95c30129d763d84da74ab106d8f724b7ca8da059
Matei Donisa <mdonisa8105@stu.d214.org>
2025-04-11 13:29:29 -0500

    Addded answer variable and made a textfield with the answer

--- ContentView.swift
+++ ContentView.swift
@@ -2,8 +2,11 @@ import SwiftUI
 struct ContentView: View {
     @State var number1 = 0
     @State var number2 = 0
+    @State var answer = 0
     var body: some View {
         TextField("enter a number", value: $number2, format: .number)
         TextField("Enter Another Number", value: $number1, format: .number)
+        Text("\(answer)")
+        
     }
 }
