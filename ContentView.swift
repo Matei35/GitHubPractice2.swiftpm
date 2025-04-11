4fb126f6f736a6d98f1173f04f8d1aae349ce1c5
Leo Jovanov <ljovanov7002@stu.d214.org>
2025-04-11 13:25:34 -0500

    Created textfield for number1

--- ContentView.swift
+++ ContentView.swift
@@ -4,5 +4,6 @@ struct ContentView: View {
     @State var number2 = 0
     var body: some View {
         TextField("enter a number", value: $number2, format: .number)
+        TextField("Enter Another Number", value: $number1, format: .number)
     }
 }
