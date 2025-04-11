4a9bf258ff6425426116e9c3a56ea5b79ff6cd0f
Leo Jovanov <ljovanov7002@stu.d214.org>
2025-04-11 13:18:12 -0500

    Created text field that is connected to number 1.

--- ContentView.swift
+++ ContentView.swift
@@ -1,8 +1,8 @@
 import SwiftUI
 struct ContentView: View {
     @State var number1: Int
-    @State var number2: Int
+    @State var number2 = 0
     var body: some View {
-       
+        TextField("Enter Number", value: $number1, format: .number)
     }
 }
